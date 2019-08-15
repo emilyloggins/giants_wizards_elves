092923c2450374bbaf01ea7daca5202991ca1b77
Emily Loggins <em.loggins7@gmail.com>
2019-08-15 18:25:29 -0500

    switch case 1-3

--- Program.cs
+++ Program.cs
@@ -1,15 +1,6 @@
 ﻿using System;
 
-namespace rock_paper_scissors
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+
 namespace rock_paper_scissors {
     class Program {
         static void Main(string[] args)
@@ -19,18 +10,21 @@ namespace rock_paper_scissors {
             Console.BackgroundColor = ConsoleColor.DarkCyan;
 
             // Initialize both scores as 0
-            int computerScore;
-            int playerScore;
+            int computerScore = 0;
+            int playerScore = 0;
 
             // Create variables to hold input from both players
-            int computerInput;
-            int playerInput;
+            // string computerInput;
+            string playerInput = Console.ReadLine();
+            // int playerInput;
 
             // Create a random number and store it in a variable to use as the computer's input generator.
-            int randomNum = random.Next(1,4);
+            Random rnd = new Random();
+            int randomNum = rnd.Next(1,4);
 
             while (true) {
-                Console.WriteLine("GIANTS BEAT ELVES, ELVES BEAT WIZARD, WIZARDS BEAT GIANTS.")
+                Console.WriteLine("GIANTS BEAT ELVES, ELVES BEAT WIZARD, WIZARDS BEAT GIANTS.");
+                Console.WriteLine();
                 Console.WriteLine("CHOOSE YOUR FIGHTER.");
                 Console.WriteLine("1 - GIANT");
                 Console.WriteLine("2 - WIZARD");
@@ -38,25 +32,65 @@ namespace rock_paper_scissors {
 
             switch (randomNum)
             {
-                case 1: 
-                computerInput = 1;
-                Console.WriteLine("COMPUTER CHOSE GIANT");
-                if (inputPlayer == 1)
-                {
-                    Console.WriteLine("BEEP BOOP, IT'S A DRAW");
+                    case 1: 
+                    // computerInput = "1";
+                    Console.WriteLine("COMPUTER CHOSE GIANT");
+                    // string playerInput = Console.ReadLine();
+                    if (playerInput == "1")
+                        {
+                            Console.WriteLine("BEEP BOOP, IT'S A DRAW");
+                        }
+                    else if (playerInput == "3")
+                        {
+                            Console.WriteLine("YOU WIN!\n\n");
+                            playerScore++;
+                        }
+                    else if (playerInput == "2")
+                        {
+                            Console.WriteLine("COMPUTER WINS BEEP BOOP\n\n");
+                            computerScore++;
+                        }
+                    break;
+                    case 2:
+                    // computerInput = "2";
+                    Console.WriteLine("COMPUTER CHOSE WIZARD");
+                    if (playerInput == "1")
+                        {
+                            Console.WriteLine("YOU WIN!\n\n");
+                            playerScore++;
+                        }
+                    else if (playerInput == "2")
+                        {
+                            Console.WriteLine("BEEP BOOP, IT'S A DRAW");
+                        }
+                    else if (playerInput == "3")
+                        {
+                            Console.WriteLine("COMPUTER WINS BEEP BOOP\n\n");
+                            computerScore++;
+                        }
+                    break;
+                    case 3:
+                    // computerInput = "3";
+                    Console.WriteLine("COMPUTER CHOSE ELF");
+                    if (playerInput == "1")
+                            {
+                                Console.WriteLine("COMPUTER WINS BEEP BOOP\n\n");
+                                computerScore++;
+                            }
+                    else if (playerInput == "2")
+                            {
+                                Console.WriteLine("YOU WIN!\n\n");
+                                playerScore++;
+                            }
+                    else if (playerInput == "PAPER")
+                            {
+                               Console.WriteLine("BEEP BOOP, IT'S A DRAW");
+                            }
+                    break;
+                    default:
+                        Console.WriteLine("PLAY BY THE RULES PUNK.");
+                        break;
                 }
-                else if (inputPlayer == 3)
-                {
-                    Console.WriteLine("YOU WIN\n\n");
-                    playerScore++;
-                }
-                else if (inputPlayer == 2)
-                {
-                    Console.WriteLine("COMPUTER WINS BEEP BOOP\n\n");
-                    computerScore++;
-                }
-                break;
-            }
             }
         }
     }
